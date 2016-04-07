49a07f11a622a37b270eb66ecb07634b3af08a02
Gabriel Anderson <zettablade@gmail.com>
2016-04-07 06:04:02 +0000

    Page specific titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | GabeCode"
   end
 
   test "should get resume" do
     get :resume
     assert_response :success
+    assert_select "title", "Resume | GabeCode"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | GabeCode"
   end
 
 end
