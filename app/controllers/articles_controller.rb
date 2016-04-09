ba0296044cd2ec7c90a87d898c29ac1e7f828339
Gabriel Anderson <zettablade@gmail.com>
2016-04-09 23:41:28 +0000

    Add edit post feature.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
-    before_action :logged_in_user, only: [:new, :create, :destroy]
+    before_action :logged_in_user, only: [:new, :create, :destroy, :edit, :update]
     
     def new
+        @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def index
@@ -19,12 +24,22 @@ class ArticlesController < ApplicationController
         redirect_to @article
     end
     
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    
     def destroy
         @article = Article.find(params[:id])
         
         @article.destroy
         flash[:success] = "Article deleted"
-        redirect_to request.referrer || root_url
+        redirect_to articles_path
     end
     
     private
